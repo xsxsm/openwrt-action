4a1b3a911f11a6577249e4958325fe6e170e9e11
vivivi <32857774+xsxsm@users.noreply.github.com>
2023-01-01 20:46:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,9 +14,10 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/shidahuilang/openwrt-package package/
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+echo 'src-git openwrt-package https://github.com/shidahuilang/openwrt-package' >>feeds.conf.default
 git clone https://github.com/immortalwrt/openwrt-filebrowser.git package/luci-app-filebrowser
 git clone https://github.com/vernesong/OpenClash.git  package/luci-app-openclash
 git clone https://github.com/thinktip/luci-theme-neobird.git  package/luci-theme-neobird
